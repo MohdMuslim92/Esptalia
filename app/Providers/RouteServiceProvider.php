4d575faac1f41bc89fcd000430edc3b793a1ea6f
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-11-07 17:05:07 +0300

    Update app/Providers/RouteServiceProvider.php
    - Add route for redirect to Healthcare provider dashboard

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
+    public const HealthcareDashboard = '/HealthcareDashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
