9c2a27b421d3b6f44b40900a13337206d6270de6
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-11-07 17:52:08 +0300

    Update app/Providers/RouteServiceProvider.php
    - Update the route for healthcare provider dashboard to the correct
      route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
-    public const HealthcareDashboard = '/HealthcareDashboard';
+    public const HealthcareDashboard = '/Healthcare-Provider';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
